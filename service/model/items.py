fd703867b8575122cf58ea38034933fc24d6ad4f
Linkun Wan <lw2861@nyu.edu>
2025-03-03 02:38:43 -0500

    item database

---
+++ service/model/items.py
@@ -0,0 +1,55 @@
+import logging
+from .models import db, PersistentBase, DataValidationError
+
+logger = logging.getLogger("flask.app")
+
+
+# Item table
+class Item(db.Model, PersistentBase):
+    """
+    Class that represents an Item
+    """
+
+    # table fields
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(256))
+    price = db.Column(db.Float, nullable=False)
+    # remaining
+    stock = db.Column(db.Integer, nullable=False, default=0)
+    # which order contains this item
+    order_id = db.Column(
+        db.Integer, db.ForeignKey("order.id", ondelete="CASCADE"), nullable=True
+    )
+
+    def __repr__(self):
+        return f"<Item {self.name} id=[{self.id}] price={self.price}>"
+
+    # to dict
+    def serialize(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "stock": self.stock,
+            "order_id": self.order_id,
+        }
+
+    # to object
+    def deserialize(self, data: dict) -> None:
+        try:
+            self.name = data["name"]
+            self.description = data.get("description", "")
+            self.price = float(data["price"])
+            self.stock = int(data["stock"])
+            self.order_id = data.get("order_id")
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Item: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Item: incorrect data type " + str(error)
+            ) from error
+        return self
